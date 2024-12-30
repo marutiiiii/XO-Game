c5e1d4e0dcaca5417dbaf7f69b00fa97b1e13804
Maruti Nimbalkar <maruti.nimbalkar_24uds@sanjivani.edu.in>
2024-12-30 07:44:34 +0530

    Update app.js

--- X O game/app.js
+++ X O game/app.js
@@ -91,3 +91,95 @@ const checkWinner = () => {
 
 newbtn.addEventListener("click", resetgame);
 resetbtn.addEventListener("click", resetgame);
+
+
+
+
+
+
+
+
+1. Install Git:
+
+sudo apt update
+sudo apt install git -y
+
+2. Install JDK 21:
+
+sudo apt update
+sudo apt install openjdk-21-jdk -y
+
+3. If not available, download from Adoptium or Oracle:
+
+wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
+sudo dpkg -i jdk-21_linux-x64_bin.deb
+sudo apt-get install -f  # To fix dependencies
+
+3.1. Set JDK 21 as default:
+
+sudo update-alternatives --config java
+java -version
+
+4. Install Jenkins:
+
+Add Jenkins repository and install:
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins -y
+
+Start Jenkins service:
+
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+Verify Jenkins is running:
+
+sudo systemctl status Jenkins
+
+Step 1:
+
+git init
+
+git add .
+
+git commit -m "Initial Commit"
+
+Step 2:
+
+git remote add origin repo_url
+
+git branch -M main or master
+
+git push -u origin main or master
+
+
+
+Now Start from Jenkins............
+
+1] after setting up Jenkins open it , go to manage Jenkins on left side
+
+2] then click on System (Configure global ...) and scroll down till GitHub
+
+3] in name give anything such as JCICD , API URL is already there then,
+   for credentials click on add and click Jenkins
+
+4] Kind select Secret Text and in Secret Paste the token generated in GitHub,
+   name id anything such as JCICD and click on add
+
+5] now in credentials select the id you enter JCICD and click on Test connection,
+   if you get Credentials verified then integrations is done successfully now save it 
+
+6] then click on new item , select freestyle name it anything , 
+   then for source code management click git 
+
+7] in URL paste repo link in branch change the branch
+
+8] in build trigger click Poll SCM and type * * * * * 
+
+9] then in build steps choose Execute shell and add javac CICD.java , java CICD
+   then click on save and click on build now 
+
+10] then again click on new item click pipeline , 
+    scroll down and in script paste the script and save and build
